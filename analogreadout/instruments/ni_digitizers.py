66a5a0a994644d9ff819e9b0666407a944f72f32
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-01-02 16:08:36 -0800

    added take_pulse_data method for ni6120 digitizer

--- analogreadout/instruments/ni_digitizers.py
+++ analogreadout/instruments/ni_digitizers.py
@@ -1,6 +1,7 @@
 import PyDAQmx
 import numpy as np
 from time import sleep
+from scipy.ndimage.filters import median_filter
 
 DAQmx_Val_Rising = PyDAQmx.DAQmxConstants.DAQmx_Val_Rising
 DAQmx_Val_FiniteSamps = PyDAQmx.DAQmxConstants.DAQmx_Val_FiniteSamps
@@ -27,19 +28,25 @@ class NI6120:
         self.sample_rate = 8.0e5  # samples per second
         # set default number of samples per channel
         self.samples_per_channel = 2e4
+        # collect more data than requested at a time to trigger on pulses
+        self.trigger_factor = 20
 
     def initialize(self, application, channels=None, sample_rate=None, num_samples=None):
         if application == "pulse_data":
-            pass
-
+            # collect more data than requested at a time to trigger on pulses
+            if num_samples is None:
+                num_samples = self.trigger_factor * self.samples_per_channel
+            else:
+                num_samples = self.trigger_factor * num_samples
         elif application == "noise_data":
-            self._create_channels(channels=channels)
-            self._configure_sampling(sample_rate=sample_rate, num_samples=num_samples)
-            self._set_channel_coupling()
-            self._disable_aa_filter()
-
+            pass
         elif application == "sweep_data":
             pass
+        self._create_channels(channels=channels)
+        self._set_channel_coupling()
+        self._disable_aa_filter()
+        self._configure_sampling(sample_rate=sample_rate, num_samples=num_samples)
+
         
     def take_noise_data(self, n_triggers):
         data = []
@@ -53,6 +60,55 @@ class NI6120:
             for channel_index in range(n_channels):
                 data[channel_index][index, :] = sample[channel_index, :]
         return tuple(data)
+        
+    def take_pulse_data(self, n_triggers, n_sigma=4):
+        # initialize data array
+        n_channels = len(self.channels)
+        n_samples = int(self.samples_per_channel / self.trigger_factor)
+        data = np.zeros((n_channels, n_triggers, n_samples))
+        # compute triggers
+        sigmas = self._find_sigma_levels(search_length=500)
+        # collect data
+        n_pulses = 0
+        while n_pulses < n_triggers:
+            sample = self._acquire_readings().T 
+            sample -= np.median(sample, axis=1)
+            # time ordered pulse indices
+            time_indices, _ = np.where(np.abs(sample) > n_sigma * sigmas)
+            # enforce one trigger per n_samples
+            logic = np.ones(time_indices.shape, dtype=bool)            
+            for index, time_index in enumerate(time_indices):
+                if np.any(time_index - time_indices[:index] < n_samples):
+                    logic[index] = False
+            # enforce no triggers at the beginning or end of sample
+            logic = np.logical_and(logic, time_indices > n_samples / 2) 
+            logic = np.logical_and(
+                logic, time_indices < self.samples_per_channel - n_samples / 2)        
+            # trim triggers and convert to index array
+            time_indices = time_indices[logic]
+            index_array = (np.ones((len(time_indices), n_samples)) *
+                           np.arange(-n_samples // 2, n_samples // 2) +
+                           np.atleast_2d(time_indices).T)
+            # add triggers to data array
+            new_data = sample[index_array]  # (triggers, samples, channel)
+            new_data = np.transpose(new_data, (2, 0, 1))  # (channel, triggers, samples)
+            n_new_pulses = new_data.shape[1]
+            if n_new_pulses + n_pulses > n_triggers:
+                n_new_pulses = n_triggers - n_pulses
+            data[:, n_pulses:n_pulses + n_new_pulses, :] = new_data[:, :n_new_pulses, :] 
+            # update counter
+            n_pulses += n_new_pulses
+        return tuple(data)           
+            
+    
+    def take_iq_point(self):
+        channel_data = self._acquire_readings()
+        # combine I and Q signals
+        data = np.zeros(int(len(channel_data) / 2))
+        for index in range(int(len(channel_data) / 2)):
+            data[index] = (np.median(channel_data[2 * index]) +
+                           1j * np.median(channel_data[2 * index + 1]))
+        return data
 
     def reset(self):
         """
@@ -61,6 +117,26 @@ class NI6120:
         error = PyDAQmx.DAQmxResetDevice(self.device)
         return error
 
+    def _find_sigma_levels(self, search_length=50):
+        # define some sizes
+        n_kernel = int(self.samples_per_channel / self.trigger_factor)
+        n_kernel = n_kernel // 2 * 2 + 1  # round up to odd number for kernel size
+        n_samples = self.samples_per_channel - (n_kernel - 1)
+        n_channels = len(self.channels)
+        boundary = (n_kernel - 1) / 2
+        # compute the median absolute deviation for a bunch of acquisitions
+        mad_data = np.ones((n_channels, n_samples * search_length)) * np.inf
+        for index in range(search_length):
+            data = self._acquire_readings()
+            s = slice(index * n_samples, (index + 1) * n_samples)
+            mad_data[s] = median_filter(np.abs(data - np.median(data, axis=1)),
+                                        (1, n_kernel))[:, boundary:-boundary]
+        # the index with the smallest median absolute deviation has the least
+        # pulse contamination
+        indices = np.argmin(mad_data)
+        sigmas = 1.4826 * mad_data[range(mad_data.shape[0]), indices]
+        return sigmas
+        
     def _create_channels(self, channels=None):
         """
         Creates four channels by default.
