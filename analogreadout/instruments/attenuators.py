074b27b10db301f5510907a97cd3725f6de59697
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-01-07 12:11:54 -0800

    changed attenuator class so that you can chain channels together

--- analogreadout/instruments/attenuators.py
+++ analogreadout/instruments/attenuators.py
@@ -4,9 +4,11 @@ import numpy as np
 from time import sleep
 from analogreadout.custom_warnings import ConnectionWarning
 
-
+    
 class Weinschel83102042F:
-    def __init__(self, address):
+    MAX_ATTEN = 62
+    STEP = 2
+    def __init__(self, address, channels):
         try:
             resource_manager = visa.ResourceManager()
         except:
@@ -17,34 +19,47 @@ class Weinschel83102042F:
         print("Model Number:", identity[1])
         print("Serial Number:", identity[2])
         print("System Version:", identity[3])
-
+        self.channels = channels  # list of chained channels to get more attenuation range
+        
     def initialize(self, attenuation):
         self.reset()
         self.set_attenuation(attenuation)
 
     def set_attenuation(self, attenuation):
-        self.write("CHAN 2")
-        self.write("ATTN 0")
-        self.write("CHAN 1")
-        if np.isinf(attenuation) and attenuation > 0:
-            self.write("CHAN 2")
-            self.write("ATTN 62")
-            self.write("CHAN 1")
-            self.write("ATTN 62")
-        elif attenuation > 62:
-            warnings.warn("setting at 62 dB, the max attenuation", UserWarning)
-            self.write("ATTN 62")
+        # check for step size compatibility
+        if (attenuation % self.STEP) != 0 and not np.isinf(attenuation):
+            new_attenuation = min(self.MAX_ATTEN * len(self.channels),
+                                  attenuation - attenuation % self.STEP) 
+            message = "setting at {:.0f} dB, must be divisible by {:d}"
+            warnings.warn(message.format(new_attenuation, self.STEP))                        
+            return self.set_attenuation(new_attenuation)             
+        # set the attenuation
+        if attenuation > self.MAX_ATTEN * len(self.channels):
+            warnings.warn("setting at {} dB, the max attenuation"
+                          .format(self.MAX_ATTEN * len(self.channels)), UserWarning)
+            for channel in self.channels:
+                self._write_channel(channel)
+                self._write_attenuation(self.MAX_ATTEN)
         elif attenuation < 0:
             warnings.warn("setting at 0 dB, the min attenuation", UserWarning)
-            self.write("ATTN 0")
-        elif (attenuation % 2) != 0:
-            message = "setting at {:.0f} dB, only attenuations divisible by 2 are allowed"
-            warnings.warn(message.format(attenuation - attenuation % 2))
-            self.write("ATTN {}".format(attenuation - attenuation % 2))
+            for channel in self.channels:
+                self._write_channel(channel)
+                self._write_attenuation(0)
         else:
-            self.write("ATTN {}".format(attenuation))
+            current_attenuation = 0
+            for index, channel in enumerate(self.channels):
+                self._write_channel(channel)
+                new_attenuation = min(self.MAX_ATTEN, attenuation - current_attenuation)
+                self._write_attenuation(new_attenuation)
+                current_attenuation += new_attenuation
         sleep(2)
-
+    
+    def _write_attenuation(self, attenuation):
+        self.write("ATTN {:d}".format(attenuation))
+        
+    def _write_channel(self, channel):
+        self.write("CHAN {:d}".format(channel))
+    
     def write(self, *args, **kwargs):
         self.session.write(*args, **kwargs)
 
