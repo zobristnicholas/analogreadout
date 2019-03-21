7c95ade181257406786d0216e4c7c5770ad91a4b
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-03-21 15:20:00 -0700

    modified todo

--- analogreadout/instruments/resistance_bridges.py
+++ analogreadout/instruments/resistance_bridges.py
@@ -49,9 +49,7 @@ class LakeShore370AC(LS370):
             
     @property
     def temperature(self):
-        # TODO: log the temperature and resistance every time it is measured to a special log file that can be set up by
-        #  each procedure. The procedure can remove and add new file handlers so that the logs follow where the data is
-        #  saved.
+        # TODO: it would be nice to write to the 'temperature' log here
         temp = self.input[self.channel - 1].kelvin
         sleep(self.WAIT_MEASURE)
         return temp
