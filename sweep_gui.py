f745c92bf98c6121c3ad81ab022a448a7616e682
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-09-24 19:28:11 -0700

    sweep_gui now has JPL configuration. Command line switching still not implemented

--- sweep_gui.py
+++ sweep_gui.py
@@ -3,9 +3,9 @@ import sys
 import numpy as np
 from pymeasure.display.Qt import QtGui
 from analogreadout.daq import DAQ
-from analogreadout.procedures import Sweep2
-from mkidplotter import (SweepGUI, SweepGUIProcedure2, SweepPlotWidget, NoisePlotWidget, TransmissionPlotWidget,
-                         TimePlotIndicator, get_image_icon)
+from analogreadout.procedures import Sweep1, Sweep2
+from mkidplotter import (SweepGUI, SweepGUIProcedure1, SweepGUIProcedure2, SweepPlotWidget, NoisePlotWidget,
+                         TransmissionPlotWidget, TimePlotIndicator, get_image_icon)
          
 import pulse_gui                
 # from pulse_gui import pulse_window, temperature, setup_logging, daq, indicators
@@ -41,31 +41,56 @@ def open_pulse_gui(self, experiment):
     self.pulse_window.show()
         
 
-def sweep_window():
-    # setup options
-    x_list = (('i1', 'i1_bias'), ('f1',), ('f1_psd', 'f1_psd'), ('i2', 'i2_bias'), ('f2',), ('f2_psd', 'f2_psd'))
-    y_list = (('q1', 'q1_bias'), ('t1',), ("i1_psd", "q1_psd"), ('q2', 'q2_bias'), ('t2',), ("i2_psd", "q2_psd"))
-    x_label = ("I [V]", "frequency [GHz]", "frequency [Hz]", "I [V]", "frequency [GHz]", "frequency [Hz]")
-    y_label = ("Q [V]", "|S21| [dBm]", "PSD [V² / Hz]", "Q [V]", "|S21| [dBm]", "PSD [V² / Hz]")
-    legend_list = (('loop', 'bias point'), None, ('I', 'Q'),
-                   ('loop', 'bias point'), None, ('I', 'Q'))
-    widgets_list = (SweepPlotWidget, TransmissionPlotWidget, NoisePlotWidget,
-                    SweepPlotWidget, TransmissionPlotWidget, NoisePlotWidget)
-    names_list = ('Channel 1: IQ', 'Channel 1: |S21|', 'Channel 1: Noise',
-                  'Channel 2: IQ', 'Channel 2: |S21|', 'Channel 2: Noise')
-    indicators = TimePlotIndicator(pulse_gui.time_stamps, pulse_gui.temperatures, title='Device Temperature [mK]')
-    pulse_gui.temperature_updater = pulse_gui.TemperatureUpdater()
-    # patch the function to open the pulse gui
-    SweepGUI.open_pulse_gui = open_pulse_gui    
-    # make the window
-    w = SweepGUI(Sweep2, base_procedure_class=SweepGUIProcedure2, x_axes=x_list,
-                 y_axes=y_list, x_labels=x_label, y_labels=y_label,
-                 legend_text=legend_list, plot_widget_classes=widgets_list,
-                 plot_names=names_list, log_level="INFO", persistent_indicators=indicators)
-    # connect the daq to the process after making the window so that the log widget gets
-    # the instrument creation log messages
-    pulse_gui.daq = DAQ("UCSB")
-    Sweep2.connect_daq(pulse_gui.daq)
+def sweep_window(config="UCSB"):
+    if config == "UCSB":
+        # setup options
+        x_list = (('i1', 'i1_bias'), ('f1',), ('f1_psd', 'f1_psd'), ('i2', 'i2_bias'), ('f2',), ('f2_psd', 'f2_psd'))
+        y_list = (('q1', 'q1_bias'), ('t1',), ("i1_psd", "q1_psd"), ('q2', 'q2_bias'), ('t2',), ("i2_psd", "q2_psd"))
+        x_label = ("I [V]", "frequency [GHz]", "frequency [Hz]", "I [V]", "frequency [GHz]", "frequency [Hz]")
+        y_label = ("Q [V]", "|S21| [dBm]", "PSD [V² / Hz]", "Q [V]", "|S21| [dBm]", "PSD [V² / Hz]")
+        legend_list = (('loop', 'bias point'), None, ('I', 'Q'),
+                       ('loop', 'bias point'), None, ('I', 'Q'))
+        widgets_list = (SweepPlotWidget, TransmissionPlotWidget, NoisePlotWidget,
+                        SweepPlotWidget, TransmissionPlotWidget, NoisePlotWidget)
+        names_list = ('Channel 1: IQ', 'Channel 1: |S21|', 'Channel 1: Noise',
+                      'Channel 2: IQ', 'Channel 2: |S21|', 'Channel 2: Noise')
+        indicators = TimePlotIndicator(pulse_gui.time_stamps, pulse_gui.temperatures, title='Device Temperature [mK]')
+        pulse_gui.temperature_updater = pulse_gui.TemperatureUpdater()
+        # patch the function to open the pulse gui
+        SweepGUI.open_pulse_gui = open_pulse_gui    
+        # make the window
+        w = SweepGUI(Sweep2, base_procedure_class=SweepGUIProcedure2, x_axes=x_list,
+                     y_axes=y_list, x_labels=x_label, y_labels=y_label,
+                     legend_text=legend_list, plot_widget_classes=widgets_list,
+                     plot_names=names_list, log_level="INFO", persistent_indicators=indicators)
+        # connect the daq to the process after making the window so that the log widget gets
+        # the instrument creation log messages
+        pulse_gui.daq = DAQ("UCSB")
+        Sweep2.connect_daq(pulse_gui.daq)
+    elif config == "JPL":
+        # setup options
+        x_list = (('i', 'i_bias'), ('f',), ('f_psd', 'f_psd'))
+        y_list = (('q', 'q_bias'), ('t',), ("i_psd", "q_psd"))
+        x_label = ("I [V]", "frequency [GHz]", "frequency [Hz]")
+        y_label = ("Q [V]", "|S21| [dBm]", "PSD [V² / Hz]")
+        legend_list = (('loop', 'bias point'), None, ('I', 'Q'))
+        widgets_list = (SweepPlotWidget, TransmissionPlotWidget, NoisePlotWidget)
+        names_list = ('IQ', '|S21|', 'Noise')
+        indicators = TimePlotIndicator(pulse_gui.time_stamps, pulse_gui.temperatures, title='Device Temperature [mK]')
+        pulse_gui.temperature_updater = pulse_gui.TemperatureUpdater()
+        # patch the function to open the pulse gui
+        SweepGUI.open_pulse_gui = open_pulse_gui    
+        # make the window
+        w = SweepGUI(Sweep1, base_procedure_class=SweepGUIProcedure1, x_axes=x_list,
+                     y_axes=y_list, x_labels=x_label, y_labels=y_label,
+                     legend_text=legend_list, plot_widget_classes=widgets_list,
+                     plot_names=names_list, log_level="INFO", persistent_indicators=indicators)
+        # connect the daq to the process after making the window so that the log widget gets
+        # the instrument creation log messages
+        pulse_gui.daq = DAQ("JPL")
+        Sweep1.connect_daq(pulse_gui.daq)
+    else:
+        raise ValueError("'{}' is not a valid configuration".format(config))
     return w
 
 
@@ -74,7 +99,7 @@ if __name__ == '__main__':
     pulse_gui.setup_logging()
     app = QtGui.QApplication(sys.argv)
     app.setWindowIcon(get_image_icon("loop.png"))
-    window = sweep_window()
+    window = sweep_window("JPL")
     window.activateWindow()
     window.show()
     ex = app.exec_()
