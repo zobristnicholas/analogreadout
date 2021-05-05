abd7d69af8ffbfa3245f865e7bdc5a76415296e9
SKYNET\mazinlab <nzobrist@physics.ucsb.edu>
2021-05-05 11:53:33 -0700

    thermometer improvements

--- analogreadout/instruments/resistance_bridges.py
+++ analogreadout/instruments/resistance_bridges.py
@@ -1,3 +1,6 @@
+import os
+import yaml
+import pyvisa
 import logging
 import threading
 import numpy as np
@@ -21,8 +24,12 @@ LOCK = threading.Lock()
 class LakeShore370AC(LS370):
     WAIT_MEASURE = 0.15
 
-    def __init__(self, address, channel, scanner=None):
-        self.channel = channel
+    def __init__(self, address, thermometer, scanner=None):
+        file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..",
+                                 "configurations", thermometer.lower() + ".yaml")
+        with open(file_name, "r") as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+        self.channel = self.config['channel']
         transport = Visa(address, parity=Parity.odd, data_bits=7)
         super().__init__(transport, scanner=scanner)
         identity = self.identification
@@ -39,38 +46,41 @@ class LakeShore370AC(LS370):
         # set channel and turn autoscan off
         self.input.scan = self.channel, False
         sleep(self.WAIT_MEASURE)
-        # lowest bias setting (may be modified to a different setting later)
-        if self._set_point is not None and self._set_point > 100:
-            self.set_bias(3, 'voltage')
-        else:
-            self.set_bias(2, 'voltage')
+        self.set_bias()
             
     @property
     def temperature(self):
         with LOCK:
-            temp = self.input[self.channel - 1].kelvin
-            sleep(self.WAIT_MEASURE)
-            temperature_log.info(f"{temp * 1000:g} mK")
-            return temp
+            try:
+                temp = self.input[self.channel - 1].kelvin
+                sleep(self.WAIT_MEASURE)
+                temperature_log.info(f"{temp * 1000:g} mK")
+            except pyvisa.VisaIOError:
+                log.error("The {:s} {:s}, s/n: {:s}, version: {:s} ".format(*self.identity)
+                          + "failed to measure the temperature.", exc_info=True)
+                temp = np.nan
+        return temp
     
     @property
     def resistance(self):
         with LOCK:
-            res = self.input[self.channel - 1].resistance
-            sleep(self.WAIT_MEASURE)
-            resistance_log.info(f"{res:g} Ohm")
-            return res
+            try:
+                res = self.input[self.channel - 1].resistance
+                sleep(self.WAIT_MEASURE)
+                resistance_log.info(f"{res:g} Ohm")
+            except pyvisa.VisaIOError:
+                log.error("The {:s} {:s}, s/n: {:s}, version: {:s} ".format(*self.identity)
+                          + "failed to measure the resistance.", exc_info=True)
+                res = np.nan
+        return res
         
     def set_temperature(self, temperature, heater_range=5, wait=30, stop=None):
         if wait <= 0 or self.calibration(temperature) <= 0 or self._set_point == temperature:
             return
         log.debug("Setting temperature to {} mK".format(temperature))
         self._set_point = temperature
-        self.set_range(heater_range)
-        if temperature < 100:
-            self.set_bias(2, 'voltage')
-        else:
-            self.set_bias(3, 'voltage')
+        self.set_heater_range(heater_range)
+        self.set_bias()  # uses self._set_point and self.config to determine the bias parameters
         n_sleep = 0
         self.set_heater_output(self.calibration(temperature))
         while n_sleep < wait:
@@ -86,7 +96,7 @@ class LakeShore370AC(LS370):
                 if callable(stop) and stop():
                     return
                 
-    def set_range(self, heater_range=5):
+    def set_heater_range(self, heater_range=5):
         self.heater.range = self.heater.RANGE[heater_range]
         sleep(self.WAIT_MEASURE)
         
@@ -94,35 +104,57 @@ class LakeShore370AC(LS370):
         self.heater.manual_output = level
         sleep(self.WAIT_MEASURE)
         
-    def set_bias(self, index, mode='voltage', auto_range=True):
+    def set_bias(self, **kwargs):
         """
-        Voltage Mode    Current Mode
-        1 2.00 μV       1 1.00 pA
-        2 6.32 μV       2 3.16 pA
-        3 20.0 μV       3 10.0 pA
-        4 63.2 μV       4 31.6 pA
-        5 200 μV        5 100 pA
-        6 632 μV        6 316 pA
-        7 2.00 mV       7 1.00 nA
-        8 6.32 mV       8 3.16 nA
-        *9 20.0 mV      9 10.0 nA
-        *10 63.2 mV     10 31.6 nA
-        *11 200 mV      11 100 nA
-        *12 632 mV      12 316 nA
-                        13 1.00 μA
-                        14 3.16 μA
-                        15 10.0 μA
-                        16 31.6 μA
-                        17 100 μA
-                        18 316 μA
-                        19 1.00 mA
-                        20 3.16 mA
-                        21 10.0 mA
-                        22 31.6 mA
+        Voltage Mode    Current Mode    Resistance Range
+        1 2.00 μV       1 1.00 pA       1  2.00 mOhm
+        2 6.32 μV       2 3.16 pA       2  6.32 mOhm
+        3 20.0 μV       3 10.0 pA       3  20.0 mOhm
+        4 63.2 μV       4 31.6 pA       4  63.2 mOhm
+        5 200 μV        5 100 pA        5  200 mOhm
+        6 632 μV        6 316 pA        6  632 mOhm
+        7 2.00 mV       7 1.00 nA       7  2.00 Ohm
+        8 6.32 mV       8 3.16 nA       8  6.32 Ohm
+        *9 20.0 mV      9 10.0 nA       9  20.0 Ohm
+        *10 63.2 mV     10 31.6 nA      10 63.2 Ohm
+        *11 200 mV      11 100 nA       11 200 Ohm
+        *12 632 mV      12 316 nA       12 632 Ohm
+                        13 1.00 μA      13 2.00 kOhm
+                        14 3.16 μA      14 6.32 kOhm
+                        15 10.0 μA      15 20.0 kOhm
+                        16 31.6 μA      16 63.2 kOhm
+                        17 100 μA       17 200 kOhm
+                        18 316 μA       18 632 kOhm
+                        19 1.00 mA      19 2.00 MOhm
+                        20 3.16 mA      20 6.32 MOhm
+                        21 10.0 mA      21 20.0 MOhm
+                        22 31.6 mA      22 63.2 MOhm
         """
-        settings = self.input[self.channel - 1].resistance_range
-        r_range = settings[2]
-        self.input[self.channel - 1].resistance_range = mode, index, r_range, auto_range, False
+        # Get the bias settings from the config file based on the set point
+        for temperature_range in self.config['ranges']:
+            if self._set_point is None:
+                break
+            if temperature_range['start'] <= self._set_point < temperature_range['stop']:
+                settings = temperature_range['bias']
+                break
+        else:  # didn't find a valid temperature range so we use the lowest start temperature
+            settings = min(self.config['ranges'], key=lambda x: x['start'])['bias'].copy()
+
+        # Override the settings with any provided kwargs.
+        settings.update(kwargs)
+
+        # Set default values for some of the settings.
+        if settings.get('resistance', None) is None:
+            resistance_range = self.input[self.channel - 1].resistance_range
+            settings['resistance'] = resistance_range[2]
+        if settings.get('mode', None) is None:
+            settings['mode'] = 'voltage'
+        if settings.get('auto_range', None) is None:
+            settings['auto_range'] = True
+
+        # Send the command to the device.
+        command = (settings['mode'], settings['index'], settings['resistance'], settings['auto_range'], False)
+        self.input[self.channel - 1].resistance_range = command
         sleep(self.WAIT_MEASURE)
         
     def close(self):
@@ -156,10 +188,13 @@ class NotAThermometer(LS370):
     def initialize(self):
         pass
 
+    def set_bias(self, **kwargs):
+        pass
+
     def set_temperature(self, temperature, heater_range=5, max_wait=60, min_wait=10, stop=None):
         pass
     
-    def set_range(self, heater_range=5):
+    def set_heater_range(self, heater_range=5):
         pass
         
     def set_heater_output(self, level):
